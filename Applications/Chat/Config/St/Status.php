b7edf3d122ea4c4e426faa38b64c75775db0759f
root <root@CentOS.(none)>
2015-12-30 22:51:18 +0800

    dir change

--- Applications/Chat/Config/St/Status.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    namespace Config\St;
-    class Status {
-        //是否允许多用户同时在线    false:允许     true：不允许
-        const NOT_ALLOW_CLIENTS = true;
-    }
-?>
--- Applications/Chat/Config/St/Storekey.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    namespace Config\St;
-    class Storekey {
-        //在线用户的redis键值
-        const USER_ONLINE_LIST = "USER_ONLINE_LIST";
-    }
-?>
