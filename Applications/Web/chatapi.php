223614e9a07f9fe576068ff043d2c63b7a01836f
root <root@CentOS.(none)>
2016-01-05 04:05:00 +0800

    入口文件接口变化

--- Applications/Web/chatapi.php
+++ Applications/Web/chatapi.php
@@ -4,8 +4,25 @@
     require '../../Workerman/Autoloader.php';
     Autoloader::setRootPath("../");
     
-    require '../Api/Apiconfig.php';
+    //require '../Api/Apiconfig.php';
+    //class and method
     $class = rtrim('Api\Controler\ ').ucfirst($_REQUEST['c']);
     $method= "do".ucfirst($_REQUEST['a']);
-    $data = Api\ApiConfig::run($class, $method); 
+    
+    ApiConfig::run($class, $method); 
+    
+    class ApiConfig {
+        /**
+         * 执行API的方法
+         */
+        public static function run($class, $method){
+            if($method === "do" || !$class) return false;
+            if(!class_exists($class))
+                exit('class '.$class.' is not found!');
+            $obj = new $class();
+            if(!method_exists($obj, $method))
+                exit('class '.$class.' has not method '.$method.' ');
+            return $obj->$method();
+        }
+    }
 ?>
