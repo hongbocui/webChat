2362dff8516540282e6da78b4a1b0a69f75ce016
root <root@CentOS.(none)>
2016-01-06 05:21:39 +0800

    解耦和

--- Applications/Config/St/Storekey.php
+++ Applications/Config/St/Storekey.php
@@ -1,7 +1,9 @@
 <?php 
     namespace Config\St;
     class Storekey {
-        //在线用户的redis键值
+        //在线用户的redis hash
         const USER_ONLINE_LIST = "USER_ONLINE_LIST";
+        
+        
     }
 ?>
